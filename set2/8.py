402d3c6ff122783bd1e502ad9215d75d1eb41203
wiktor <wiktor.sedzimir@gmail.com>
2023-10-20 20:59:13 +0200

    Add 8.py

---
+++ set2/8.py
@@ -0,0 +1,45 @@
+# this function returns next number that cannot be created as the sum of following numbers in Fibonacci sequence
+def nextNotSum(number):
+    if number < 9:
+        return 9
+    
+    fibPrevious = 1
+    fibNext = 1
+
+    while fibNext <= number:
+        fibNext, fibPrevious = fibPrevious + fibNext, fibNext
+    #end while
+
+    # if we were given with e.g. 16 then our sequence looks like that:
+    # 1  1  2  3  5  8  13          16        21              22
+    #                   ^           ^         ^               ^
+    #                   |           |         |               |
+    #             fibPrevious     number   fibNext          result
+    #
+    # we set now our solution is somewhere between 16 and 21 OR it is 22
+
+    sum = 0
+    result = fibNext + 1
+
+    while sum - number < 2:
+        fibPrevious, fibNext = fibNext - fibPrevious, fibPrevious
+        sum += fibPrevious
+    #end while
+
+    # after changes our sequence looks like that:
+    # 1  1  2       3  5  8  13         16            17            18
+    #       ^       ^                   ^             ^             ^
+    #       |       |                   |             |             |
+    #    fibPrev  fibNext              number    searched number    sum
+
+    if fibPrevious != 1:
+        result = sum - fibPrevious + 1
+        if result <= number:
+            result = number + 1    
+
+    return result
+        
+
+if __name__ == "__main__":
+    number = int(input("enter number: "))
+    print(nextNotSum(number))
