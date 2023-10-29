f3564f241cf5c9c8be037838421331bcf7fff753
wiktor <wiktor.sedzimir@gmail.com>
2023-10-29 10:33:51 +0100

    Updated 20.py

--- set3/20.py
+++ set3/20.py
@@ -1,37 +1,85 @@
+from math import isqrt
 
 # increment 1 - if we add -1 - if we subtract
 def updateDivsToTab(divs, number, increment):
     assert(number > 1)
     div = 2
-    while 1 != number:
+    iroot = isqrt(number)
+    while div <= iroot:
         while number % div == 0:
             divs[div] += increment
             number //= div
         #end while
+        iroot = isqrt(number)
         div += 1
+    #end while
+    if number > 1:
+        divs[number] += increment
+    #end if
     return divs
 
-def maxLen(numArray):
+def maxLenWindow(numArray):
     divs = [0 for _ in range(1000)]
     arrayLen = len(numArray)
     myMaxLen = 0
-    windowStart = 0
-    windowLen = 1
-    while windowStart + windowLen - 1 < arrayLen:
-        windowEnd = windowStart + windowLen - 1
-        currentLen = 0
-        while windowEnd < arrayLen:
+    windowStart, windowEnd = 0, 0
+    while windowEnd < arrayLen:
+        if max(divs) <= 1:
             divs = updateDivsToTab(divs, numArray[windowEnd], 1)
-            if max(divs) > 1:
-                break
-            #end if
-            windowLen = windowEnd - windowStart + 1
-            currentLen += 1
-            myMaxLen = max(myMaxLen, currentLen)
             windowEnd += 1
-        #end while
-        divs = updateDivsToTab(divs, numArray[windowStart], -1)
-        windowStart += 1
+        else:
+            divs = updateDivsToTab(divs, numArray[windowStart], -1)
+            windowStart += 1
+        #end if
+        if max(divs) <= 1:
+            myMaxLen = max(myMaxLen, windowEnd - windowStart)
+        #end if
+    #end while
+    return myMaxLen
+
+def maxLen(numArray):
+    arrayLen = len(numArray)
+    left, right = 0, 0
+    myMaxLen = 0
+    divs = [0 for _ in range(1000)]
+    while right < arrayLen:
+        if max(divs) <= 1:
+            currentNumber = numArray[right]
+            iroot = isqrt(currentNumber)
+            div = 2
+            while div <= iroot:
+                while currentNumber % div == 0:
+                    divs[div] += 1
+                    currentNumber //= div
+                #end while
+                div += 1
+                iroot = isqrt(currentNumber)
+            #end while
+            
+            if currentNumber > 1:
+                divs[currentNumber] += 1
+            #end if
+            right += 1
+        else:
+            currentNumber = numArray[left]
+            iroot = isqrt(currentNumber)
+            div = 2
+            while div <= iroot:
+                while currentNumber % div == 0:
+                    divs[div] -= 1
+                    currentNumber //= div
+                #end while
+                div += 1
+                iroot = isqrt(currentNumber)
+            #end while
+            
+            if currentNumber > 1:
+                divs[currentNumber] -= 1
+            #end if
+            left += 1
+        #end if
+        if max(divs) <= 1:
+            myMaxLen = max(right - left, myMaxLen)
         #end if
     #end while
     return myMaxLen
@@ -39,3 +87,4 @@ def maxLen(numArray):
 if __name__ == "__main__":
     numArray = [2,23,33,35,7,4,6,7,5,11,13,22]
     print(maxLen(numArray))
+    print(maxLenWindow(numArray))
