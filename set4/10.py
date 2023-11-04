bf7aa8cb203dd2043584ff9f9cc8967ba607cf04
wiktor <wiktor.sedzimir@gmail.com>
2023-11-04 12:24:02 +0100

    Update 10.py

--- set4/10.py
+++ set4/10.py
@@ -1,3 +1,7 @@
+##################
+# first solution #
+##################
+
 def isEveryWhereZero(zeroInRows, zeroInColls):
     tabLen = len(zeroInColls)
     result = True
@@ -24,6 +28,38 @@ def zeroInEveryCollAndRow(tab):
     
     return True if isEveryWhereZero(zeroInRows, zeroInColls) else False
 
+###################
+# second solution #
+###################
+
+def zeroInEveryCollAndRowV2(tab):
+    tabLen = len(tab)
+    isZero = True
+    for rowColl in range(tabLen):
+        isInRow = False
+        for row in range(tabLen):
+            if tab[row][rowColl] == 0:
+                isInRow = True
+            #end if
+        #end for
+        if not isInRow:
+            isZero = False
+            break
+        #end if
+        isInColl = False
+        for coll in range(tabLen):
+            if tab[rowColl][coll] == 0:
+                isInColl = True
+            #end if
+        #end for
+        if not isInColl:
+            isZero = False
+            break
+        #end if
+    #end for
+    
+    return isZero
+
 if __name__ == "__main__":
     tab = [[1,0,3,4,5],
            [1,4,27,0,5],
@@ -31,7 +67,8 @@ if __name__ == "__main__":
            [0,5,2,4,3],
            [1,1,1,1,0]]
     
-    print(zeroInEveryCollAndRow(tab))
+    print("version 1:",zeroInEveryCollAndRow(tab))
+    print("version 2:", zeroInEveryCollAndRowV2(tab))
 
     tab = [[1,0,3,4,5],
            [1,4,27,0,5],
@@ -39,7 +76,8 @@ if __name__ == "__main__":
            [1,5,2,4,3],
            [1,1,1,1,0]]
     
-    print(zeroInEveryCollAndRow(tab))
+    print("version 1:",zeroInEveryCollAndRow(tab))
+    print("version 2:", zeroInEveryCollAndRowV2(tab))
 
     tab = [[0,0,3,4,5],
            [1,4,0,0,0],
@@ -47,4 +85,5 @@ if __name__ == "__main__":
            [0,5,0,4,3],
            [1,0,1,1,0]]
     
-    print(zeroInEveryCollAndRow(tab))
+    print("version 1:",zeroInEveryCollAndRow(tab))
+    print("version 2:", zeroInEveryCollAndRowV2(tab))
