22b9808b1ea27f35d5e491f617e092ed845d3549
wiktor <wiktor.sedzimir@gmail.com>
2023-11-06 19:25:20 +0100

    Updated findNs.py

--- lectures/findNs.py
+++ lectures/findNs.py
@@ -1,15 +1,17 @@
 cntr = 0
 
-def findNs(tab, n, targetProduct, elementIndex = 0):
+def findNs(tab, n, targetProduct, factors, elementIndex = 0):
     global cntr
     if n == 1:
         for i in range(elementIndex, len(tab)):
             if tab[i] == targetProduct:
-                cntr +=1
+                factors2 = factors.copy() + [('index: '+ str(i), 'value: ' + str(tab[i]))]
+                print(factors2)
+                cntr += 1
     else:
         for i in range(elementIndex, len(tab)):
             if targetProduct % tab[i] == 0:
-                findNs(tab, n - 1, targetProduct//tab[i], i + 1)
+                findNs(tab, n - 1, targetProduct//tab[i], factors.copy() +[('index: '+ str(i), 'value: ' + str(tab[i]))], i + 1)
 
 
 
@@ -17,5 +19,5 @@ if __name__ == "__main__":
     tab = [7,12,7,15,12,2,3,2,3,4,7,45,4,9,10,5]
     targetProduct = 180
     n = 3
-    findNs(tab, n, targetProduct)
+    findNs(tab, n, targetProduct, [])
     print(cntr)
