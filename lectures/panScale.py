f2d799bd769c79b1ce088f6423bf16d685c1e855
wiktor <wiktor.sedzimir@gmail.com>
2023-11-06 19:13:29 +0100

    Updated panScale.py

--- lectures/panScale.py
+++ lectures/panScale.py
@@ -4,10 +4,10 @@ def panScale(scale, weight, scaleTabIndex, usedScales):
         return True
     if scaleTabIndex == len(scale) or weight < 0:
         return False
-    possible = panScale(scale, weight - scale[scaleTabIndex], scaleTabIndex + 1, usedScales + ', ' + str(scale[scaleTabIndex])) or panScale(scale, weight, scaleTabIndex + 1, usedScales) or panScale(scale, weight + scale[scaleTabIndex], scaleTabIndex + 1, usedScales + ', ' + str(scale[scaleTabIndex]))
+    possible = panScale(scale, weight - scale[scaleTabIndex], scaleTabIndex + 1, usedScales.copy() + [scale[scaleTabIndex]]) or panScale(scale, weight, scaleTabIndex + 1, usedScales.copy()) or panScale(scale, weight + scale[scaleTabIndex], scaleTabIndex + 1, usedScales.copy() + [-scale[scaleTabIndex]])
     return possible
 
 if __name__ == "__main__":
     scale = [1,3,5,10,16,24]
     for weight in range(1,20):
-        print(f"weight {weight}:", panScale(scale, weight, 0, ''))
+        print(f"weight {weight}:", panScale(scale, weight, 0, []))
