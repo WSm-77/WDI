101b6d74f5a541ed318d5cb60c3dd3c533f0b14c
wiktor <wiktor.sedzimir@gmail.com>
2023-11-06 21:32:00 +0100

    Added second solution

--- set4/1.py
+++ set4/1.py
@@ -1,3 +1,7 @@
+##################
+# first solution #
+##################
+
 def fillTableSpiraly(tab):
     tabLen = len(tab)
     cntr = 1
@@ -23,9 +27,47 @@ def fillTableSpiraly(tab):
         firstEdge += 1
         secondEdge -= 1
     #end while
+    print(*tab, sep="\n", end='\n\n')
+
+###################
+# second solution #
+###################
+
+def fillTableRecursively(tab):
+    tabLen = len(tab)
+    assert(tabLen >= 0)
+    def fillRecursively(tab, tabSize, tabStart, number):
+        if tabSize == 0:
+           return
+         #end if
+        if tabSize == 1:
+            tab[tabStart][tabStart] = number
+            return
+        #end if
+        tabEdge = tabStart + tabSize - 1
+        for i in range(tabStart, tabEdge):
+           tab[tabStart][i] = number
+           number += 1
+        #end for
+        for j in range(tabStart, tabEdge):
+            tab[j][tabEdge] = number
+            number += 1
+        #end for
+        for k in range(tabEdge, tabStart, -1):
+            tab[tabEdge][k] = number
+            number += 1
+        #end for
+        for l in range(tabEdge, tabStart, -1):
+            tab[l][tabStart] = number
+            number += 1
+        #end for
+        fillRecursively(tab, tabSize - 2, tabStart + 1, number)
+    fillRecursively(tab, tabLen, 0, 1)
     print(*tab, sep="\n")
 
 if __name__ == "__main__":
     n = 7
     tab = [[0 for _ in range(n)] for _ in range(n)]
+    tab2 = [[0 for _ in range(n)] for _ in range(n)]
     fillTableSpiraly(tab)
+    fillTableRecursively(tab2)
