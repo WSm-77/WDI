b562f459cbbdd633ad8d074b8e367ab106d575d9
wiktor <wiktor.sedzimir@gmail.com>
2023-11-18 12:39:35 +0100

    Add 8.py

---
+++ set6/8.py
@@ -0,0 +1,19 @@
+def pan_scale(scales, weigth, index = 0):
+    if weigth == 0:
+        return True
+    if index == len(scales) or weigth < 0:
+        return False
+    
+    return pan_scale(scales, weigth, index + 1) or \
+            pan_scale(scales, weigth - scales[index], index + 1) or \
+            pan_scale(scales, weigth + scales[index], index + 1)
+
+if __name__ == "__main__":
+    scales = [1,2,5,7,11,20]
+    for w in range(1, 51):
+        possible = ""
+        if pan_scale(scales, w):
+            possible = "possible" 
+        else:
+            possible = "not possible"
+        print(f"weight {w}: {possible}")
