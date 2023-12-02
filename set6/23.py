bafd5e944285740b505846f528df99eb62086945
wiktor <wiktor.sedzimir@gmail.com>
2023-12-02 13:51:00 +0100

    Updated 23.py

--- set6/23.py
+++ set6/23.py
@@ -4,9 +4,14 @@ def in_parrarel(res1, res2):
 def in_series(res1, res2):
     return res1 + res2
 
-def resistance(T, targetResistance, index = 0, currentResistance = 0, numberOfUsedRestistors = 0):
+def resistance(T, targetResistance, index = 0, currentResistance = 0, numberOfUsedRestistors = 0, used = []):
     if numberOfUsedRestistors == 3:
         if currentResistance == targetResistance:
+            print()
+            for use in used:
+                print(*use)
+            #end for
+            print()
             return True
         else:
             return False
@@ -17,12 +22,12 @@ def resistance(T, targetResistance, index = 0, currentResistance = 0, numberOfUs
         return False
     #end if
 
-    if  resistance(T, targetResistance, index + 1, in_series(currentResistance, T[index]), numberOfUsedRestistors + 1) or \
-        resistance(T, targetResistance, index + 1, currentResistance, numberOfUsedRestistors):
+    if  resistance(T, targetResistance, index + 1, in_series(currentResistance, T[index]), numberOfUsedRestistors + 1, used + [("serial", T[index])]) or \
+        resistance(T, targetResistance, index + 1, currentResistance, numberOfUsedRestistors, used):
             return True
     #end if
 
-    if currentResistance != 0 and resistance(T, targetResistance, index + 1, in_parrarel(currentResistance, T[index]), numberOfUsedRestistors + 1):
+    if currentResistance != 0 and resistance(T, targetResistance, index + 1, in_parrarel(currentResistance, T[index]), numberOfUsedRestistors + 1, used + [("parrarel", T[index])]):
             return True
     #end if
 
@@ -31,5 +36,5 @@ def resistance(T, targetResistance, index = 0, currentResistance = 0, numberOfUs
 if __name__ == "__main__":
     resistors = [1,8,6,9,3,2,5,7]
 
-    for targetRes in range(50):
+    for targetRes in range(30):
         print(f"resisitance {targetRes + 1}: {resistance(resistors, targetRes + 1)}")
