1901cf5a2a63d12642869fcd36eebd18856a3c55
wiktor <wiktor.sedzimir@gmail.com>
2023-12-04 13:21:33 +0100

    Add 29.py

---
+++ set6/29.py
@@ -0,0 +1,46 @@
+from random import randint
+
+def valid_distance(r, point):
+    return point[0]**2 + point[1]**2 + point[2]**2 <= r**2
+
+def center_of_mass(T, r):
+    found = False
+    def rek(T, r, index = 0, sumOfMasses = (0,0,0), numberOfUsedPoints = 0):
+
+        nonlocal found
+
+        if numberOfUsedPoints >= 3 and valid_distance(r, (sumOfMasses[0] / numberOfUsedPoints, \
+                                                          sumOfMasses[1] / numberOfUsedPoints, \
+                                                          sumOfMasses[2] / numberOfUsedPoints)):     # we don't divide by 0, cuz sumOfMasses >= 3
+            found = True
+            return
+        #end if
+
+        if index == len(T):
+            return
+        #end if
+
+        currentPoint = T[index]
+        rek(T, r, index + 1, (sumOfMasses[0] + currentPoint[0], sumOfMasses[1] + currentPoint[1], sumOfMasses[2] + currentPoint[2]), numberOfUsedPoints + 1)
+
+        if not found:
+            rek(T, r, index + 1, sumOfMasses, numberOfUsedPoints)
+        #end if
+
+    #end def
+    rek(T, r)
+
+    return found
+            
+if __name__ == "__main__":
+    tab = [(-1, 0, -2), (17, 45, 20), (2, 4, 6), (42, 5, 18), (-4, 8, -2)]
+    r = 5
+    print(*tab)
+    print("r =", r, "\b:", center_of_mass(tab, r))
+
+    tab = [(randint(0, 20), randint(-20, 20), randint(0, 20)) for _ in range(15)]
+    print(*tab, sep="\n")
+    for r in range(1, 10 + 1):
+        print("r =", r, "\b:", center_of_mass(tab, r))
+
+
