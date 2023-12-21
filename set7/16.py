02bdb3ce75a0b2d69bf3a5d5601090d070b31761
wiktor <wiktor.sedzimir@gmail.com>
2023-12-21 15:17:42 +0100

    moving almost keeps order

--- set7/16.py
+++ set7/16.py
@@ -6,7 +6,7 @@ import usefuleFunctions as uf
 def even_number_of_5_in_octal(num):
     cnt5 = 0
     while num > 0:
-        if num % 5 == 0:
+        if num % 8 == 5:
             cnt5 += 1
         #end if
         num //= 8
@@ -14,24 +14,33 @@ def even_number_of_5_in_octal(num):
     return cnt5 % 2 == 0
 
 def move_to_beginning(g) -> uf.Node:
-    ptr = g
+    if g.next == None:
+        return g
+    # end if
+    endOf5Odd = g
+    ptr = g.next
     while ptr.next != None:
+        print("pointer value:", ptr.val)
         if even_number_of_5_in_octal(ptr.next.val):
             tmp = ptr.next.next
-            ptr.next.next = g.next
-            g.next = ptr.next
+            ptr.next.next = endOf5Odd.next
+            endOf5Odd.next = ptr.next
+            endOf5Odd = endOf5Odd.next
             ptr.next = tmp
+            if endOf5Odd == ptr:
+                ptr = ptr.next
         else:
             ptr = ptr.next
         #end if
+        uf.print_guradian_list(g)
     #end while
     return g
 
 if __name__ == "__main__":
     tab = [i for i in range(15)]
-    for i in range(len(tab)):
-        print(i, even_number_of_5_in_octal(tab[i]))
-    #end for
+    # for i in range(len(tab)):
+    #     print(i, even_number_of_5_in_octal(tab[i]))
+    # #end for
     myList = uf.list_to_linked_list(tab)
     uf.print_guradian_list(myList)
     myList = move_to_beginning(myList)
