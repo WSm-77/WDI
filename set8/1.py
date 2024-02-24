831e64c60f4202ad410f57e03144e6136535fcd1
wiktor <wiktor.sedzimir@gmail.com>
2024-02-24 18:06:09 +0100

    Added print_tree_it_sorted function to 1.py

--- set8/1.py
+++ set8/1.py
@@ -39,12 +39,51 @@ def print_tree_it(ptr: fun.Node) -> None:
 			stack.append(current_node.right)
 		#end if
 	#end while
+	print("\n")
+
+def print_tree_it_sorted(ptr: fun.Node) -> None:
+	if ptr == None:
+		print("tree is empty")
+		return
+	
+	midStack = fun.deque()
+	leftStack = fun.deque()
+	rightStack = fun.deque()
+	leftStack.append(ptr)
+
+	while True:
+		while leftStack:
+			currentLeftNode = leftStack.pop()
+			if currentLeftNode.left == currentLeftNode.right == None:
+				print(currentLeftNode.val, end=" ")
+				break
+			#end if
+			if currentLeftNode.left != None:
+				leftStack.append(currentLeftNode.left)
+			#end if
+			if currentLeftNode.right != None:
+				rightStack.append(currentLeftNode.right)
+			#end if
+			midStack.append(currentLeftNode)
+		#end while
+		if midStack:
+			currenMidNode = midStack.pop()
+			print(currenMidNode.val, end=" ")
+		if currenMidNode.right != None and rightStack:
+			topRight = rightStack.pop()
+			leftStack.append(topRight)
+		#end if
+		if not midStack and not leftStack and not rightStack:
+			break
+		#end if
+	#end while
 	print()
      
 ########### 	main 	###########
 
 if __name__ == "__main__":
 	my_list = [fun.randint(1,10) for _ in range(fun.randint(8, 15))]
+	# my_list = [5,2,1,4,3,6,7]
 	print("list:")
 	print(*my_list, sep=" ", end="\n\n")
 	root = fun.list_to_BST(my_list)
@@ -56,3 +95,5 @@ if __name__ == "__main__":
 	# print iteratively
 	print("tree iteratively:")
 	print_tree_it(root)
+	print("tree iteratively (sorted):")
+	print_tree_it_sorted(root)
